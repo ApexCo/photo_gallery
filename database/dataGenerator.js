ef7d3c2da9cd04315dc76445a5c2bd19d91d0665
Adam Brent <adam.brent@gmail.com>
2021-02-06 14:07:27 -0800

    Updates README and includes schema file.

---
+++ database/dataGenerator.js
@@ -0,0 +1,99 @@
+const faker = require('faker');
+const fs = require('fs');
+
+const generateGallery = (property_id) => {
+  const photos = [];
+  const total = Math.floor(Math.random() * 16) + 5;
+  const start = Math.floor(Math.random() * 1000) + 1;
+  for (let i = 1; i <= total; i += 1) {
+    photos.push({
+      property_id,
+      photo_name: faker.random.words(),
+      photo_url: `https://abphotos.s3-us-west-2.amazonaws.com/image${(start + i) % 1000}.jpg`,
+      photo_description: faker.random.words(Math.floor(Math.random() * 4) + 2),
+      is_verified: faker.random.boolean(),
+    });
+  }
+  if (property_id % 10000 === 0) {
+    console.log(`Created gallery #${property_id}`);
+  }
+  return photos;
+};
+
+const generateProperty = () => {
+  const property = {
+    title: faker.random.words(Math.floor(Math.random()) + 2),
+    reviews: Math.floor(Math.random() * (30) + 10),
+    rating: Math.random() * 2 + 4,
+    is_superhost: faker.random.boolean(),
+    city: faker.address.city(),
+    state: faker.address.state(),
+    country: faker.address.country(),
+  };
+  return property;
+};
+
+const photoWriter = fs.createWriteStream('/users/adambrent/Desktop/allPhotos.csv');
+photoWriter.write('property_id,photo_name,photo_url,photo_description,is_verified\n', 'utf8');
+
+function writeAllPhotos(totalGalleries, writer, encoding = 'utf8', callback) {
+  let i = 0;
+  function write() {
+    let ok = true;
+    do {
+      i += 1;
+      const gallery = generateGallery(i);
+      let data = '';
+      for (let photo = 0; photo < gallery.length; photo++) {
+        let { property_id, photo_name, photo_url, photo_description, is_verified } = gallery[photo];
+        data += `${property_id},${photo_name},${photo_url},${photo_description},${is_verified},\n`;
+      }
+      if (i === totalGalleries) {
+        writer.write(data, encoding, callback);
+      } else {
+      // see if we should continue, or wait
+      // don't pass the callback, because we're not done yet.
+        ok = writer.write(data, encoding);
+      }
+    } while (i < totalGalleries && ok);
+    if (i < totalGalleries) {
+    // had to stop early!
+    // write some more once it drains
+      writer.once('drain', write);
+    }
+  }
+  write();
+}
+
+const propertyWriter = fs.createWriteStream('/users/adambrent/Desktop/properties.csv');
+propertyWriter.write('title,reviews,rating,is_superhost,city,state,country\n', 'utf8');
+
+function writeAllProperties(totalProperties, writer, encoding = 'utf8', callback) {
+  let i = 0;
+  function write() {
+    let ok = true;
+    do {
+      i += 1;
+      const property = generateProperty();
+      let data = '';
+      let { title, reviews, rating, is_superhost, city, state, country } = property;
+      data += `${title},${reviews},${rating.toFixed(2)},${is_superhost},${city},${state},${country},\n`;
+      if (i === totalProperties) {
+        writer.write(data, encoding, callback);
+      } else {
+      // see if we should continue, or wait
+      // don't pass the callback, because we're not done yet.
+        ok = writer.write(data, encoding);
+      }
+    } while (i < totalProperties && ok);
+    if (i < totalProperties) {
+    // had to stop early!
+    // write some more once it drains
+      writer.once('drain', write);
+    }
+  }
+  write();
+}
+
+writeAllPhotos(1000000, photoWriter, 'utf8', () => { console.log('Done with photos!'); });
+writeAllProperties(1000000, propertyWriter, 'utf8', () => { console.log('Done with properties!'); });
---
+++ database/schema.sql
@@ -0,0 +1,54 @@
+CREATE TABLE properties (
+  id serial,
+  title VARCHAR(100) NOT NULL,
+  reviews int DEFAULT 0,
+  rating int DEFAULT null,
+  is_superhost BOOLEAN DEFAULT false,
+  city VARCHAR(100) NOT NULL,
+  stateLoc VARCHAR(25) NOT NULL,
+  country VARCHAR(50) NOT NULL,
+  created_date timestamp DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+);
+
+CREATE TABLE photos (
+  id serial,
+  property_id int NOT NULL,
+  photo_name VARCHAR(100),
+  photo_url VARCHAR(100),
+  thumbnail_url VARCHAR(100) DEFAULT NULL,
+  photo_description VARCHAR(100),
+  is_verified BOOLEAN DEFAULT false,
+  created_date timestamp DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (property_id) REFERENCES properties(id) ON DELETE CASCADE,
+);
+
+CREATE TABLE users (
+  id serial,
+  username VARCHAR(30) NOT NULL,
+  pw VARCHAR(20) NOT NULL,
+  firstname VARCHAR(30) NOT NULL,
+  lastname VARCHAR(50) NOT NULL,
+  created_date timestamp DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+);
+
+CREATE TABLE property_lists (
+  id serial,
+  user_id int NOT NULL
+  title VARCHAR(30) NOT NULL,
+  created_date timestamp DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+);
+
+CREATE TABLE property_list_properties (
+  id serial,
+  list_id int NOT NULL
+  property_id int NOT NULL
+  created_date timestamp DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (list_id) REFERENCES property_lists(id) ON DELETE CASCADE,
+  FOREIGN KEY (property_id) REFERENCES properties(id) ON DELETE CASCADE,
+);
