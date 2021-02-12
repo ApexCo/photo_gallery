8ac3ca35ea1db5e0080a10ffe0097878d42f790e
Adam Brent <adam.brent@gmail.com>
2021-02-12 15:17:28 -0800

    Fixes new routes to use correct methods.

--- server/index.js
+++ server/index.js
@@ -16,9 +16,18 @@ app.get('/api/properties/:id', controllers.getPropertyById);
 app.get('/api/photos/:id', controllers.getPhotoById);
 app.post('/api/properties', controllers.addItem);
 app.post('/api/photos', controllers.addItem);
+app.post('/api/users', controllers.addItem);
+app.post('/api/property_lists', controllers.addItem);
+app.post('/api/property_list_properties', controllers.addItem);
 app.delete('/api/properties/:id', controllers.deleteItem);
 app.delete('/api/photos/:id', controllers.deleteItem);
+app.delete('/api/users/:id', controllers.deleteItem);
+app.delete('/api/property_lists/:id', controllers.deleteItem);
+app.delete('/api/property_list_properties/:id', controllers.deleteItem);
 app.patch('/api/properties/:id', controllers.updateItem);
 app.patch('/api/photos/:id', controllers.updateItem);
+app.patch('/api/users/:id', controllers.updateItem);
+app.patch('/api/property_lists/:id', controllers.updateItem);
+app.patch('/api/property_list_properties/:id', controllers.updateItem);
 
 app.listen(port);
